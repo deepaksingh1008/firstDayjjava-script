709cc0c498eadf4400533ebd76fabb97ff34b94d
deepaksingh <ds4850716@gmail.com>
2023-06-27 13:17:33 +0530

    first commit

---
+++ first-class.js
@@ -0,0 +1,83 @@
+//array = specific
+//object = genric
+//functional programing means  object maked inside the function
+// var a=10;
+// var b=20;
+// var c = a+b;
+// // console.log(`sum of two number is ${c}`);
+
+// a==b //loose comparsion it compare only value
+// a===b //strictly comparison its compare the datatype and value both
+
+// //conversion
+// /*
+// 1. S+N=S
+// 2. N+S=S
+// 3. Boolean+number=number
+
+// */
+// var a = "10"+10;
+// console.log(a)  //output 1010;
+// //convert into interger
+// //using parseInt() function
+// a = parseInt('10')+10 //output 20
+// // if you write 10-"10" then it automatic convert into int
+
+// a = "amit" - "deepak";
+// isNan(a) output=true
+// console.log(a) //output NaN if we perform iligle like that its return NaN(it type is number) it is Not a Number;
+
+// //number function parseInt,parseFloat,isNaN;
+
+// a = 10/0; 
+// console.log(a) //output infinity
+// isFinite(e)?e:0; its return true if it is finite
+
+//String + number = string
+
+// boolean+number = number;
+
+// true+true-false output = 2 true=1,false=0 it return number
+// true+"name"=String output= truename;
+
+// var a =10293746323804n //it is bigInt if number is very large
+// console.log(typeof a)
+
+//we make big int two way
+//1.hardcoded  write at the end 1723763n;
+//2.by using BigInt(); if dynamic value
+
+// var a  = BigInt("16738343894990505");
+// console.log(typeof a)
+
+//var a = null; ,it type is object it is use deallocate the memory 
+//how to free memory
+//1.reReference
+//2.null 
+//3.make under a scope
+
+
+//home-work  null vs undefined and [] vs {}
+
+
+// var arr = [1,22,3,45,6];
+
+// console.log(typeof arr) //output object
+
+// arr instanceof Array return true
+// arr instanceof Object return true
+
+//console.log(Object)
+
+
+
+
+
+
+
+
+
+
+
+
+
